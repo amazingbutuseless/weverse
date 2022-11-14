3ef6f3e9ef87d082aac77aea5100c3bfeb2d91da
amazingbutuseless <cindian@amazingbutuseless.net>
2022-11-14 21:44:22 +0900

    fix: /api/thumbnail 실행시 404 Not Found 에러가 발생하는 문제

--- apps/weverse-og-image/helpers/playwright-settings.tsx
+++ apps/weverse-og-image/helpers/playwright-settings.tsx
@@ -6,20 +6,14 @@ export const PlaywrightSettings = (() => {
   let page: playwright.Page;
 
   async function init() {
-    await chromium.font(
-      'https://fonts.gstatic.com/s/notosanskr/v15/Pby6FmXiEBPT4ITbgNA5CgmOsn7uwpY.woff2'
-    );
-    await chromium.font(
-      'https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf'
-    );
-
-    browser = await playwright.chromium.launch({
+    const options = {
       args: [...chromium.args, '--lang=ko-KR'],
       executablePath:
         (await chromium.executablePath) ||
         '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
       headless: chromium.headless,
-    });
+    };
+    browser = await playwright.chromium.launch(options);
 
     page = await browser.newPage({
       viewport: { width: 768, height: 403 },
